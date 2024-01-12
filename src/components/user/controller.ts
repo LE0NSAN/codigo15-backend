981cf3d4826623f0baa13cc05cbdf8737cd7f034
Ivan Leon <leonsanp@gmail.com>
2024-01-11 21:32:57 -0500

    Componente User agregado

--- src/components/user/controller.ts
+++ src/components/user/controller.ts
@@ -5,70 +5,70 @@ import { handleResponseError } from "../../utils";
 import { hash } from "../../crypto";
 
 export async function list(_req: Request, res: Response): Promise<Response> {
-try {
+  try {
     const users = await prisma.user.findMany();
     return responseSuccess({ res, data: users });
-} catch (error) {
+  } catch (error) {
     return handleResponseError(res, error);
-}
+  }
 }
 
 export async function getById(req: Request, res: Response): Promise<Response> {
-try {
+  try {
     const user = await prisma.user.findUnique({
-    where: {
+      where: {
         id: Number(req.params.id),
-    },
+      },
     });
 
     if (!user) {
-    return responseError({ res, data: "User not found" });
+      return responseError({ res, data: "User not found" });
     }
     return responseSuccess({ res, data: user });
-} catch (error) {
+  } catch (error) {
     return handleResponseError(res, error);
-}
+  }
 }
 
 export async function store(req: Request, res: Response): Promise<Response> {
-try {
+  try {
     req.body.password = hash(req.body.password);
 
     await prisma.user.create({ data: req.body });
 
     return responseSuccess({ res, data: "User created", status: 201 });
-} catch (error) {
+  } catch (error) {
     return handleResponseError(res, error);
-}
+  }
 }
 
 export async function update(req: Request, res: Response): Promise<Response> {
-try {
+  try {
     if (req.body.password) {
-    req.body.password = hash(req.body.password);
+      req.body.password = hash(req.body.password);
     }
 
     const user = await prisma.user.update({
-    where: { id: Number(req.params.id) },
-    data: req.body,
+      where: { id: Number(req.params.id) },
+      data: req.body,
     });
 
     if (!user) {
-    return responseError({ res, data: "User not found" });
+      return responseError({ res, data: "User not found" });
     }
 
     return responseSuccess({ res, data: "User updated" });
-} catch (error) {
+  } catch (error) {
     return handleResponseError(res, error);
-}
+  }
 }
 
 export async function destroy(req: Request, res: Response): Promise<Response> {
-try {
+  try {
     await prisma.user.delete({ where: { id: Number(req.params.id) } });
 
     return responseSuccess({ res, data: "User deleted" });
-} catch (error) {
+  } catch (error) {
     return handleResponseError(res, error);
-}
+  }
 }
